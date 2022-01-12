bf0d53c10d379ed75c72297e26a64903f911dc23
Will <wor101@gmail.com>
2022-01-11 23:01:42 -0500

    work on 3sum

--- 3sum/3sum.go
+++ 3sum/3sum.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func twoSum(nums []int, target int) []int {
 	seen := map[int]int{}
@@ -8,10 +11,12 @@ func twoSum(nums []int, target int) []int {
 	for index, value := range nums {
 		diff := target - value
 
-		valueIndex, ok := seen[diff]
+		_, ok := seen[diff]
+		// fmt.Println("valueIndex:", valueIndex)
+		// fmt.Println("Diff:", diff)
 
 		if ok {
-			return []int{value, valueIndex}
+			return []int{value, diff}
 		} else {
 			seen[value] = index
 		}
@@ -20,26 +25,82 @@ func twoSum(nums []int, target int) []int {
 	return nil
 }
 
+func allTwoSums(nums []int, target int) [][]int {
+	seen := map[int]int{}
+	var allTwoSums [][]int
+
+	for index, value := range nums {
+		diff := target - value
+
+		_, ok := seen[diff]
+		// fmt.Println("valueIndex:", valueIndex)
+		// fmt.Println("Diff:", diff)
+
+		if ok {
+			allTwoSums = append(allTwoSums, []int{value, diff})
+		} else {
+			seen[value] = index
+		}
+	}
+
+	if len(allTwoSums) > 0 {
+		return allTwoSums
+	} else {
+		return nil
+	}
+}
+
 // - model of problem:
 //   - USE THE 2SUM SOLUTION
 //   - set two pointers, start, and 2sumpointer
 //   - iterate over nums
 //     - for each num, set your target as the difference between 0 and the current num
 //     - use the 2sum solution to find the values you need from 2sumpointer to end of nums
+
+func isEqual(newTriplet []int, triplet []int) bool {
+	for i, v := range triplet {
+		if newTriplet[i] != v {
+			return false
+		}
+	}
+	return true
+}
+
+func magicallyCheckforNoDuplicateTriplet(newTriplet []int, triplets [][]int) bool {
+	sort.Ints(newTriplet)
+
+	for _, triplet := range triplets {
+		if isEqual(newTriplet, triplet) {
+			//fmt.Printf("Trip: %v is equal to %v", newTriplet, triplet)
+			return false
+		}
+	}
+	return true
+}
+
 func threeSum(nums []int) [][]int {
 	var triplets [][]int
 	start := 0
 	twoSumPointer := 1
 
-	for start < len(nums)-3 {
+	for start <= len(nums)-3 {
 		target := 0 - nums[start]
-		result := twoSum(nums[twoSumPointer:], target)
-		if result != nil {
-			newTriplet := []int{nums[start]}
-			newTriplet = append(newTriplet, result...)
-			// magicallyCheckforDuplicateTriplet
-			triplets = append(triplets, newTriplet)
+		//result := twoSum(nums[twoSumPointer:], target)
+		results := allTwoSums(nums[twoSumPointer:], target)
+
+		// fmt.Printf("Target: %v  StartValue: %v  Slice: %v  Result: %v \n", target, nums[start], nums[twoSumPointer:], results)
+		// fmt.Println("All2SUMS:", allTwoSums(nums[twoSumPointer:], target))
+		if results != nil && len(results) != 0 {
+			for _, result := range results {
+				newTriplet := []int{nums[start]}
+				newTriplet = append(newTriplet, result...)
+				if magicallyCheckforNoDuplicateTriplet(newTriplet, triplets) {
+					triplets = append(triplets, newTriplet)
+				}
+			}
+
 		}
+		fmt.Printf("2SumPoint: %v  Len-2: %v \n", twoSumPointer, len(nums)-2)
 		if twoSumPointer < len(nums)-2 {
 			twoSumPointer++
 		} else {
@@ -50,6 +111,8 @@ func threeSum(nums []int) [][]int {
 }
 
 func main() {
+	fmt.Println(threeSum([]int{0, -1, 1}))
 	fmt.Println(threeSum([]int{-1, 0, 1, 2, -1, -4})) // [[-1,-1,2],[-1,0,1]]
+	fmt.Println(threeSum([]int{-2, 0, 1, 1, 2}))      // [[-2,0,2],[-2,1,1]]
 
 }
--- two-sum/two-sum.go
+++ two-sum/two-sum.go
@@ -24,4 +24,5 @@ func main() {
 	fmt.Println(twoSum([]int{2, 7, 11, 15}, 9))
 	fmt.Println(twoSum([]int{3, 3}, 6))
 	fmt.Println(twoSum([]int{3, 2, 4}, 6))
+	fmt.Println(twoSum([]int{0, -1, 1}, 0))
 }
